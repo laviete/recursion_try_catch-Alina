0fd4779511cd69d1e3c065e743f549c95a081da4
Alina <135998117+laviete@users.noreply.github.com>
2023-07-25 23:30:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const calcFactorialRecursion = (number) => {
   return number === 0 || number === 0n ? 1n : number * calcFactorial(number - 1n);
   */
 };
-
 try {
   console.log(calcFactorialRecursion(4));
   console.log(calcFactorialRecursion(1.7)); //Error: Number incorrect - enter an integer
@@ -35,3 +34,34 @@ try {
 } finally {
   console.log("Completion of calculation");
 }
+
+
+//--------------switch---------------
+const factorial = (number) => {
+  if (!Number.isInteger(number)) {
+    throw new Error("Number incorrect - enter an integer");
+  }
+  if (number < 0) {
+    throw new Error("Number incorrect - enter a positive number");
+  }
+  if (typeof number === "number" || typeof number === "bigint") {
+    switch (typeof number) {
+      case "number":
+        return number === 0 ? 1 : number * factorial(number - 1);
+      case "bigint":
+        return number === 0n ? 1n : number * factorial(number - 1n);
+    }
+  }
+};
+try {
+  console.log(factorial(4));
+  //console.log(factorial(1.7)); //Error: Number incorrect - enter an integer
+  //console.log(factorial(-4)); //Error: Number incorrect - enter a positive number
+  //console.log(factorial("1n")); //Error: Number incorrect - enter an integer
+} catch (error) {
+  console.log(error);
+} finally {
+  console.log("Completion of calculation");
+}
+
+
